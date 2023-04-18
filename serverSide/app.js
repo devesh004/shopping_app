faf6ae263407c7e459f785eafffb83990148e3e5
Devesh Shakya <06deveshshakya@gamil.com>
2023-04-18 17:32:35 +0530

    made changes to deploy

--- serverSide/app.js
+++ serverSide/app.js
@@ -28,14 +28,14 @@ app.use("/api/products", productRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client_admin/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "client_admin", "build", "index.html")
-    );
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client_admin/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(
+//       path.resolve(__dirname, "client_admin", "build", "index.html")
+//     );
+//   });
+// }
 
 app.listen(process.env.PORT || 3001, () => {
   console.log("listing port 3001");
